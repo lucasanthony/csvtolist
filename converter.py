d808d16b9a9a447781135dbbde51932a7f0ec681
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2018-12-04 10:11:07 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,3 +9,29 @@ def csvtolist(arquivo,separador):
    		for linha in dados:
    			lista.append(linha)
 	return lista
+
+def merge(b):
+	attributes = ['Price','Year','Mileage','City','State','Vin','Make','Model']
+	retorno = []
+	for i in range (len(attributes)):
+		retorno.append(attributes[i])
+		retorno.append(b[i])
+
+	return retorno
+
+def dicionarios(a):
+	retorno = []
+	for i in a:
+		inbuffer = i
+		merged = merge(inbuffer)
+		dic = dict(merged[i:i+2] for i in range(0, len(merged), 2))
+		retorno.append(dic)
+	retorno.pop(0)
+	return retorno
+
+
+dados = csvtolist('dados',',')
+
+lib = dicionarios(dados)
+
+print lib
